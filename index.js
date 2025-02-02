fb65aeb224492cdfdcd7556b4ed86644334e0ffc
Joaov8tor <163661591+Joaov8tor@users.noreply.github.com>
2025-02-02 20:10:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,28 @@ const port = process.env.PORT || 3000;
 app.get('/api/parse', async (req, res) => {
   const { url } = req.query;
 
+  // Verifica se o parâmetro 'url' foi fornecido
   if (!url) {
-    return res.status(400).json({ error: 'URL is required' });
+    return res.status(400).json({ error: 'URL é um parâmetro obrigatório' });
   }
 
   try {
+    // Realiza a chamada à API externa
     const response = await axios.get(`https://hahabypasser-secret-or-no.vercel.app/bypass?url=${url}`);
+    
+    // Verifica se o campo 'result' está presente na resposta
     const result = response.data.result;
 
     if (result) {
       return res.json({ result });
     } else {
-      return res.status(500).json({ error: 'No result found in response' });
+      return res.status(500).json({ error: 'Resultado não encontrado na resposta da API externa' });
     }
   } catch (error) {
-    return res.status(500).json({ error: 'Error fetching data from the provided URL' });
+    return res.status(500).json({ error: 'Erro ao buscar dados da URL fornecida' });
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
